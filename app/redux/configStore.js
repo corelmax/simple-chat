c170051187195ac278f0833db3974530f2fb5471
Nattapon Rattajak <rattajak.n@gmail.com>
2018-01-09 01:09:20 +0700

    no message

--- app/redux/configStore.js
+++ app/redux/configStore.js
@@ -2,15 +2,15 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const redux_1 = require("redux");
 const rootReducer_1 = require("./rootReducer");
-exports.store = redux_1.createStore(rootReducer_1.default);
-// const middlewares = [];
-// if (process.env.NODE_ENV === `development`) {
-//     const logger = require("redux-logger").createLogger();
-//     middlewares.push(logger);
-// }
-// const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore);
-// function configureStore() {
-//     // let initialState = rootReducer.getInitialState();
-//     return createStoreWithMiddleware(rootReducer);
-// }
-// export const store = configureStore();
+// export const store = createStore(rootReducer);
+const middlewares = [];
+if (process.env.NODE_ENV === `development`) {
+    const logger = require("redux-logger").createLogger();
+    middlewares.push(logger);
+}
+const createStoreWithMiddleware = redux_1.applyMiddleware(...middlewares)(redux_1.createStore);
+function configureStore() {
+    // let initialState = rootReducer.getInitialState();
+    return createStoreWithMiddleware(rootReducer_1.default);
+}
+exports.store = configureStore();

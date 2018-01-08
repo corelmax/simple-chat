c7c98b5d25a6e9b9d9650e3d5d4d6d250cb6da4b
Nattapon Rattajak <rattajak.n@gmail.com>
2018-01-09 00:34:17 +0700

    no message

--- app/redux/configStore.ts
+++ app/redux/configStore.ts
@@ -2,15 +2,16 @@ import { createStore, applyMiddleware } from "redux";
 
 import rootReducer from "./rootReducer";
 
-const middlewares = [];
-if (process.env.NODE_ENV === `development`) {
-    const logger = require("redux-logger").createLogger();
-    middlewares.push(logger);
-}
+export const store = createStore(rootReducer);
 
-const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore);
-function configureStore() {
-    // let initialState = rootReducer.getInitialState();
-    return createStoreWithMiddleware(rootReducer);
-}
-export const store = configureStore();
+// const middlewares = [];
+// if (process.env.NODE_ENV === `development`) {
+//     const logger = require("redux-logger").createLogger();
+//     middlewares.push(logger);
+// }
+// const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore);
+// function configureStore() {
+//     // let initialState = rootReducer.getInitialState();
+//     return createStoreWithMiddleware(rootReducer);
+// }
+// export const store = configureStore();

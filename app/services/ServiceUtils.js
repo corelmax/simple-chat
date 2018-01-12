7185404047d834f48f5a5590d49e73861f8e2636
Nattapon Rattajak <rattajak.n@gmail.com>
2018-01-12 19:43:33 +0700

    use internal config.

--- app/services/ServiceUtils.js
+++ app/services/ServiceUtils.js
@@ -1,13 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const starter_1 = require("stalk-js/starter");
-const getConfig = () => starter_1.BackendFactory.getInstance().getApiConfig();
+const InternalStore_1 = require("../InternalStore");
+const getConfig = () => InternalStore_1.default.getApiConfig();
 exports.apiHeaders = () => ({
     "Content-Type": "application/json",
     "cache-control": "no-cache",
     "x-api-key": getConfig().apiKey,
     "Access-Control-Allow-Credentials": "*",
-    "Access-Control-Allow-Origin": "*"
+    "Access-Control-Allow-Origin": "*",
 });
 exports.withToken = (headers) => (token) => {
     headers["x-access-token"] = token;
--- app/services/ServiceUtils.ts
+++ app/services/ServiceUtils.ts
@@ -1,12 +1,12 @@
-import { BackendFactory } from "stalk-js/starter";
-const getConfig = () => BackendFactory.getInstance().getApiConfig();
+import InternalStore from "../InternalStore";
+const getConfig = () => InternalStore.getApiConfig();
 
 export const apiHeaders = () => ({
     "Content-Type": "application/json",
     "cache-control": "no-cache",
     "x-api-key": getConfig().apiKey,
     "Access-Control-Allow-Credentials": "*",
-    "Access-Control-Allow-Origin": "*"
+    "Access-Control-Allow-Origin": "*",
 });
 
 export const withToken = (headers: any) => (token: string) => {

2a31845be89d3b9c43ae2bf6406f1e5cb5e7f686
Nattapon Rattajak <rattajak.n@gmail.com>
2018-01-12 16:39:16 +0700

    update module declaration.

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,8 @@
 export { ChatRoomComponent, ChatsLogComponent } from "./app/index";
 export { IDataManager } from "./app/IDataManager";
-export * from "./app/redux/stalkBridge";
+export * from "./app/redux/stalkBridge/stalkBridgeActions";
+export * from "./app/redux/stalkBridge/stalkNotificationActions";
+export * from "./app/redux/stalkBridge/stalkReducer";
 export * from "./app/redux/chatroom";
 export * from "./app/redux/chatlogs";
 export * from "./app/redux/actions/chatlistsRx";
